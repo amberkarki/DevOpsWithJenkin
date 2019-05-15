a35e5881669c6baf1a99cdda67ec45fc2cd965c8
amber <amberkarki@gmail.com>
2019-05-15 02:58:22 -0700

    first commit

---
+++ src/HelloJenkins.java
@@ -0,0 +1,12 @@
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.printf("The  is  Just Test DevOps  of ICT Project and cloud  " );
+
+	}
+
+}
